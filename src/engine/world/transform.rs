dc8a70cf1fca31e45f4909cc80e4fe849f420b48
samcspohn <samuel.c.spohn@gmail.com>
2023-06-29 15:46:15 -0500

    improved logic of VecCache capacity reserve

--- src/engine/world/transform.rs
+++ src/engine/world/transform.rs
@@ -227,8 +227,8 @@ impl<'a, T> VecCache<T> {
         if let Some(mut a) = self.avail.pop() {
             let b = unsafe { &mut *a.v.get() };
             let len = b.len();
-            b.reserve(capacity.checked_sub(len).unwrap_or(0));
             b.clear();
+            b.reserve(capacity);
             a
         } else {
             // let mut a = self.store.lock();
